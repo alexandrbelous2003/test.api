9b977e4b7d53bce754fb834772b15544825893b3
v_belous <mailforbva@gmail.com>
2021-06-15 00:51:33 +0500

    add api

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from '../service/auth.service';
 
-@Controller('auth')
+@Controller('api/auth')
 export class AuthController {
 
     constructor(private authService: AuthService){
