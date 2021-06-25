0c09baea1ffe7c2adffdb0fa901289d39550cca3
alexandrbelous2003 <alexandrbelous2003@gmail.com>
2021-06-25 22:42:33 +0500

    fix -vegatables bag

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config/config.service';
+
 
 @Module({
   imports: [
     AuthModule, 
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
   ],
   controllers: [AppController],
   providers: [AppService],
